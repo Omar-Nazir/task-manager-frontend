4e83d151a023258ead7faf6ad7b85e068f3b7b8c
Omar-Nazir <backupun2941@gmail.com>
2025-08-03 16:44:31 +0500

    frontend linked

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
-const API = 'https://task-manager-backend-production-dcec.up.railway.app/tasks';
+const API = process.env.REACT_APP_URL || 5000;
 
 function App() {
   const [tasks, setTasks] = useState([]);
